ac41c41c8b2daff6abf3f466b584d0be32fd79ff
Blessing <blazeahu123@gmail.com>
2025-04-13 22:37:04 +0100

    Update app.js and other files

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.get("/deliverables", function(req, res){
 });
 
 
-app.listen(3000, function(){
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){
+    console.log(`Server running on port ${PORT}`);
 });
